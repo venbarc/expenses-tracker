45635eb5a64364d8436e1266bebd99759b28d326
venbarc <118934661+venbarc@users.noreply.github.com>
2025-08-27 23:09:55 +0800

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    
+    {{-- head --}}
+    <x-app.head />
+
+    <body class="antialiased">
+        
+       <div class="bg-danger text-white p-5"> Nav bar here</div>
+    </body>
+</html>
