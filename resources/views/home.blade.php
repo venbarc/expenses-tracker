8b9a8fd0b31577dc1cf0f7e4348c606861e99368
venbarc <118934661+venbarc@users.noreply.github.com>
2025-08-28 23:16:34 +0800

    bg color, font style , and navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,16 @@
     {{-- head --}}
     <x-app.head />
 
-    <body class="antialiased">
-        
-       <div class="bg-danger text-white p-5"> Nav bar here</div>
+    <body class="antialiased" id="main-bg">
+
+        <nav class="navbar navbar-expand-lg shadow-sm py-lg-5 py-4 bg-dark">
+            <div class="container-fluid px-lg-5 px-3 d-flex justify-content-between align-items-center">
+                <a class="navbar-brand fw-bold text-light" href="#">ExpensesTracker</a>
+                <a class="btn btn-outline-light ms-lg-3" href="#">Login</a>
+            </div>
+        </nav>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
